38aa2aa37e77f5ddd94e5f3a534d6b3c2e21181a
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-11-17 16:50:54 +0200

    refactoring

--- src/components/AdditionalMenu/index.tsx
+++ src/components/AdditionalMenu/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {useState} from 'react'
 // style
 import styles from './styles.module.scss'
-import classNames from 'classnames';
 
 import search from './../../sources/images/search.svg';
 import fav from './../../sources/images/fav.svg'
@@ -10,57 +9,37 @@ import basket from './../../sources/images/basket.svg'
 import avatar from './../../sources/images/avatar.svg'
 import other from './../../sources/images/other.svg'
 import menu from './../../sources/images/menu.svg'
-import close from './../../sources/images/close.svg'
+import Search from 'components/Search';
 
 const AdditionalMenu: React.FC = () => {
 
   const [showSearch, setShowSearch] =  useState(false);
+  
+  const onHandlerClick = () => { 
+    setShowSearch(!showSearch);
+  }
 
   let searchBox = null; 
   if(showSearch) {
-    searchBox = (
-    <div className={styles.searchContainer}>
-      <div className={styles.searchBox}>
-        <div className={styles.inputFieldContainer}>
-          <input type="text" placeholder='Search among millions of products' className={styles.inputField}/>
-          <img src={search} alt="" className={classNames(styles.icon, styles.search)}/>
-        </div>
-
-        <div className={styles.searchHistoryContainer}> 
-          <h2 className={styles.title}><b>Resent Search</b></h2>
-
-          <div className={styles.historyItem}>
-            <h3 className={styles.text}>White Thirt</h3>
-            <img src={close} alt="" className={classNames(styles.icon, styles.deleteButton)}/>
-          </div>
-          <div className={styles.historyItem}>
-            <h3 className={styles.text}>Overhit with belt</h3>
-            <img src={close} alt="" className={classNames(styles.icon, styles.deleteButton)}/>
-          </div>
-        </div>
-      </div>
-      <div onClick={()=>setShowSearch(!showSearch)}> 
-        <img src={close} alt="" className={classNames(styles.icon, styles.close)}/>
-      </div> 
-    </div>)
+    searchBox =  <Search handlerClick={onHandlerClick}/>
   }
-  
+
   return (
+      <div className={styles.menu}>
+        {searchBox}
+        <div className={styles.imageContainer} onClick={onHandlerClick}>
+          <img src={search} alt="" className={styles.icon}/>
+        </div>
 
-  <div className={styles.menu}>
-    {searchBox}
-    <div className={styles.imageContainer} onClick={()=>setShowSearch(!showSearch)}>
-      <img src={search} alt="" className={styles.icon}/>
-    </div>
+        <div className={styles.fullMenu}> 
+            <img src={fav} alt="" className={styles.icon}/>
+            <img src={basket} alt="" className={styles.icon}/>
+            <img src={avatar} alt="" className={styles.icon}/>
+            <img src={other} alt="" className={styles.icon}/>
+        </div>
+        <img src={menu} alt="" className={styles.mobMenu}/>
+      </div>  
 
-    <div className={styles.fullMenu}> 
-        <img src={fav} alt="" className={styles.icon}/>
-        <img src={basket} alt="" className={styles.icon}/>
-        <img src={avatar} alt="" className={styles.icon}/>
-        <img src={other} alt="" className={styles.icon}/>
-    </div>
-    <img src={menu} alt="" className={styles.mobMenu}/>
-  </div>  
   )
 }
 
